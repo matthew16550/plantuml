80fce22d22e9b03a8202beb5dd2296f89b5bdf4a
matthew16550 <matthew@matthew16550.com>
2021-10-12 02:15:32 +1100

    wip

--- src/net/sourceforge/plantuml/xml/XmlFactories.java
+++ src/net/sourceforge/plantuml/xml/XmlFactories.java
@@ -1,5 +1,6 @@
 package net.sourceforge.plantuml.xml;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,11 +18,43 @@ public class XmlFactories {
 	// (see https://stackoverflow.com/a/8297830/1848731)
 
 	private static class DocumentBuilderFactoryHolder {
-		static final DocumentBuilderFactory INSTANCE = DocumentBuilderFactory.newInstance();
+		static final DocumentBuilderFactory INSTANCE;
+
+		static {
+			INSTANCE = DocumentBuilderFactory.newInstance();
+			
+			// Prevent XML security problems
+			// See https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#java
+			setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			setFeature("http://xml.org/sax/features/external-general-entities", false);
+			setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			INSTANCE.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+			INSTANCE.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+			INSTANCE.setXIncludeAware(false);
+			INSTANCE.setExpandEntityReferences(false);
+		}
+
+		private static void setFeature(String name, boolean value) {
+			try {
+				INSTANCE.setFeature(name, value);
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private static class TransformerFactoryHolder {
-		static final TransformerFactory INSTANCE = TransformerFactory.newInstance();
+		static final TransformerFactory INSTANCE;
+
+		static {
+			INSTANCE = TransformerFactory.newInstance();
+
+			// Prevent XML security problems
+			// See https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#java
+			INSTANCE.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+			INSTANCE.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+		}
 	}
 
 	public static DocumentBuilder newDocumentBuilder() throws ParserConfigurationException {
