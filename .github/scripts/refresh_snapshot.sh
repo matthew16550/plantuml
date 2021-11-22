cb079c080be181c265a4b4aee6c1cd639a7852e9
matthew16550 <matthew@matthew16550.com>
2021-11-23 10:50:41 +1100

    wip

--- .github/scripts/refresh_snapshot.sh
+++ .github/scripts/refresh_snapshot.sh
@@ -1,7 +1,15 @@
 #! /usr/bin/env bash
 set -ex
+env
 
 TAG=snapshot
+
+url="https://maven.pkg.github.com/${REPO_OWNER}/plantuml/net/sourceforge/plantuml/plantuml/${RELEASE_SHA}/plantuml-${RELEASE_SHA}"
+
+# auth is read from the .netrc file
+wget -q "${url}.jar"         -O plantuml-SNAPSHOT.jar 
+wget -q "${url}-javadoc.jar" -O plantuml-SNAPSHOT-javadoc.jar 
+wget -q "${url}-sources.jar" -O plantuml-SNAPSHOT-sources.jar 
 
 gh release delete "${TAG}" || true
 git push --delete origin "${TAG}" || true
@@ -12,10 +20,6 @@ cat <<-EOF >release-notes.txt
   ⚠️  **It is not ready for general use** ⚠️
   ⏱  _Snapshot taken $(date -u +"%F at %T (UTC)")_
 EOF
-
-mv plantuml.jar         plantuml-SNAPSHOT.jar
-mv plantuml-javadoc.jar plantuml-SNAPSHOT-javadoc.jar
-mv plantuml-sources.jar plantuml-SNAPSHOT-sources.jar
 
 gh release create --prerelease --target "${RELEASE_SHA}" --title "${TAG}" --notes-file release-notes.txt "${TAG}" \
   plantuml-SNAPSHOT.jar \
---
+++ .github/scripts/refresh_snapshot_configure.js
@@ -0,0 +1,76 @@
+const RELEASE_BRANCH = "snapshot-release-2"
+
+module.exports = async ({context, core, github}) => {
+	core.info("Finding current snapshot ...")
+
+	const currentSnapshot = await findCurrentSnapshot(context, github);
+
+	core.info(`Current snapshot: ${currentSnapshot || "NONE"}`)
+
+	core.info(`Finding newest commits ...`)
+
+	const commits = await findNewestCommits(`refs/heads/${RELEASE_BRANCH}`, context, github);
+
+	for (let commit of commits) {
+		const sha = commit.oid;
+		core.info(`Considering ${commit.url}`)
+
+		if (currentSnapshot && currentSnapshot === sha) {
+			core.notice(`Snapshot is already at the newest possible commit ${commit.url}`)
+			return
+		}
+
+		if (await packageVersionExists("net.sourceforge.plantuml.plantuml", sha, context, github)) {
+			core.notice(`Updating to ${commit.url})`)
+			core.setOutput("new_sha", sha);
+			return
+		}
+	}
+
+	// We could look at more commits by paging the history() query but usually it should not be relevant so not implemented yet
+	core.warning(`Nothing found from ${commits.length} newest commits`)
+}
+
+async function findCurrentSnapshot(context, github) {
+	const currentSnapshot = (
+			await github.rest.repos.getReleaseByTag({
+				...context.repo,
+				tag: "snapshot",
+			})
+	).target_commitish  // todo null?
+	return currentSnapshot;
+}
+
+async function findNewestCommits(head, context, github) {
+	return (
+			await github.graphql(`
+          			query($owner: String!, $repo: String!, $head: String!) {
+          			  repository(owner: $owner, name: $repo) {
+          				object(expression: $head) {
+          				  ... on Commit {
+          					history(first: 100, since: $since) {
+							  nodes {
+								oid
+								url
+          			} } } } } }`,
+					{...context.repo, head}
+			)
+	).repository.object.history.nodes;
+}
+
+async function packageVersionExists(name, version, context, github) {
+	const packages = (
+			await github.graphql(`
+					query($owner: String!, $repo: String!, $name: String!, $version: String!) {
+					  repository(owner: $owner, name: $repo) {
+						packages(first: 1, names: $name) {
+						  nodes {
+							version(version: $version) {
+							  id
+					} } } } }`,
+					{...context.repo, name, version}
+			)
+	).response.repository.packages.nodes
+
+	return packages.length && packages[0].version
+}
