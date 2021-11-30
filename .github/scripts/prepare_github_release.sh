a0c0be3c8222c41b35d3d42d282511122cf14e48
matthew16550 <matthew@matthew16550.com>
2021-12-01 00:03:47 +1100

    wip

--- .github/scripts/prepare_github_release.sh
+++ .github/scripts/prepare_github_release.sh
@@ -7,13 +7,13 @@ to_dir="target/github_release_staging"
 
 mkdir "${to_dir}"
 
-# todo ln
-cp -v "${from_dir}/plantuml-${FROM_VERSION}.pom"             "${to_dir}/plantuml-${TO_VERSION}.pom"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}.pom.asc"         "${to_dir}/plantuml-${TO_VERSION}.pom.asc"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}.jar"             "${to_dir}/plantuml-${TO_VERSION}.jar"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}.jar.asc"         "${to_dir}/plantuml-${TO_VERSION}.jar.asc"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar"     "${to_dir}/plantuml-${TO_VERSION}-javadoc.jar"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-javadoc.jar.asc"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}-sources.jar"     "${to_dir}/plantuml-${TO_VERSION}-sources.jar"
-cp -v "${from_dir}/plantuml-${FROM_VERSION}-sources.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-sources.jar.asc"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}.pom"             "${to_dir}/plantuml-${TO_VERSION}.pom"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}.pom.asc"         "${to_dir}/plantuml-${TO_VERSION}.pom.asc"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}.jar"             "${to_dir}/plantuml-${TO_VERSION}.jar"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}.jar.asc"         "${to_dir}/plantuml-${TO_VERSION}.jar.asc"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar"     "${to_dir}/plantuml-${TO_VERSION}-javadoc.jar"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-javadoc.jar.asc"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}-sources.jar"     "${to_dir}/plantuml-${TO_VERSION}-sources.jar"
+ln -s "${from_dir}/plantuml-${FROM_VERSION}-sources.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-sources.jar.asc"
+
 tree target
