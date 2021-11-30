1ffd030aade6ef5e465c78bbb04a9d8d383c762f
matthew16550 <matthew@matthew16550.com>
2021-11-30 23:58:07 +1100

    wip

--- .github/scripts/prepare_github_release.sh
+++ .github/scripts/prepare_github_release.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env bash
 set -ex
+tree target
 
 from_dir="target/nexus-staging/deferred/net/sourceforge/plantuml/plantuml/${FROM_VERSION}"
 to_dir="target/github_release_staging"
 
 mkdir "${to_dir}"
 
+# todo ln
 cp -v "${from_dir}/plantuml-${FROM_VERSION}.pom"             "${to_dir}/plantuml-${TO_VERSION}.pom"
 cp -v "${from_dir}/plantuml-${FROM_VERSION}.pom.asc"         "${to_dir}/plantuml-${TO_VERSION}.pom.asc"
 cp -v "${from_dir}/plantuml-${FROM_VERSION}.jar"             "${to_dir}/plantuml-${TO_VERSION}.jar"
@@ -14,3 +16,4 @@ cp -v "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar"     "${to_dir}/plantuml
 cp -v "${from_dir}/plantuml-${FROM_VERSION}-javadoc.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-javadoc.jar.asc"
 cp -v "${from_dir}/plantuml-${FROM_VERSION}-sources.jar"     "${to_dir}/plantuml-${TO_VERSION}-sources.jar"
 cp -v "${from_dir}/plantuml-${FROM_VERSION}-sources.jar.asc" "${to_dir}/plantuml-${TO_VERSION}-sources.jar.asc"
+tree target
